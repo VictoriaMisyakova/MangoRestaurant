f5262230730576e1feb40c268b63d19125226502
Victoria Misyakova <vmisyakova@gmail.com>
2023-08-07 14:03:39 +0300

    Add automapper

--- Mango.Services.ProductAPI/Models/Dto/ProductDto.cs
+++ Mango.Services.ProductAPI/Models/Dto/ProductDto.cs
@@ -1,6 +1,14 @@
-﻿namespace Mango.Services.ProductAPI.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ProductAPI.Models.Dto
 {
     public class ProductDto
     {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
