9a471ca2c36b47a3bce7af165d5fc51deffa9df7
Victoria Misyakova <vmisyakova@gmail.com>
2023-08-08 21:05:10 +0300

    Added CRUD for product

--- Mango.Web/Services/ProductService.cs
+++ Mango.Web/Services/ProductService.cs
@@ -26,7 +26,7 @@ namespace Mango.Web.Services
             return await this.SendAsync<T>(new ApiRequest()
             {
                 ApiType = SD.ApiType.GET,
-                Url = SD.ProductAPIBase + "/api/products" + id,
+                Url = SD.ProductAPIBase + "/api/products/" + id,
                 AccessToken = ""
             });
         }
@@ -58,7 +58,7 @@ namespace Mango.Web.Services
             return await this.SendAsync<T>(new ApiRequest()
             {
                 ApiType = SD.ApiType.DELETE,
-                Url = SD.ProductAPIBase + "/api/products" + id,
+                Url = SD.ProductAPIBase + "/api/products/" + id,
                 AccessToken = ""
             });
         }
